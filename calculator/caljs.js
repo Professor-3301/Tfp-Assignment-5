906eda23afc4a48b53ae2f1488c8a4d680d8df36
Adwaith Anil <adwaith00adwaith@gmail.com>
2022-07-24 19:15:05 +0530

    second commit

---
+++ calculator/caljs.js
@@ -0,0 +1,10 @@
+function ret(i){
+    document.getElementById("head").value+=i
+    }
+
+function ers(){
+    document.getElementById("head").value=''
+}
+function res(){
+    document.getElementById("head").value=eval(document.getElementById("head").value)
+}
